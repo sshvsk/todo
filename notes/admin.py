0020fa74161a69c9da6585259e657a34d19f38e7
Alexander Vasyuk <ssh74vsk@gmail.com>
2022-08-05 21:36:36 +0300

    Add todo

---
+++ notes/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from notes.models import Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "created_at")
+    fields = ("author", "title", "text", "created_at")
+    readonly_fields = ("created_at",)
+
